4721c8fc4ca9d66dacdc3b6013a26c25489b9fd5
Yejin-source <yejin3831@naver.com>
2025-04-25 13:52:12 +0900

    [Bronze V] Title: 사칙연산, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -0,0 +1,12 @@
+# input() 함수로 입력받은 후
+# split() 함수로 나누어서 A, B 변수에 저장
+A, B = input().split()
+
+print(int(A)+int(B))
+print(int(A)-int(B))
+print(int(A)*int(B))
+print(int(A)//int(B))
+print(int(A)%int(B))
+
+# a/b : 기본 나눗셈
+# a//b : 나눗셈의 몫만 구하기
