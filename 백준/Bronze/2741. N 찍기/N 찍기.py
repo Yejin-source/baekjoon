f5bf39ee99c07d0f8fd2eaad3cf85b5ca3d32076
Yejin-source <yejin3831@naver.com>
2025-04-28 13:51:37 +0900

    [Bronze V] Title: N 찍기, Time: 100 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.py
@@ -0,0 +1,15 @@
+N = int(input())
+
+for i in range(N): # range(stop)
+    print(i + 1)
+
+      
+# for문 
+# for 변수 in 객체(리스트, 문자열 등):
+    
+# range() 함수 
+# 특정 범위의 숫자 시퀀스를 만드는 함수
+
+# range(stop)
+# range(start, stop)
+# range(start, stop, step)
