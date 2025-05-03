16f3511e731e9079028d3172f2fbc429e261cfcd
Yejin-source <yejin3831@naver.com>
2025-05-03 21:46:02 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.py
@@ -0,0 +1,14 @@
+text = input()
+
+# 대소문자 바꾸기
+print(text.swapcase())  # swapcase() 메서드
+
+
+# 대소문자를 바꾸는 메서드
+
+# swapcase(): 대소문자 반전
+# lower(): 모두 소문자로
+# upper(): 모두 대문자로
+
+# capitalize(): 첫 글자만 대문자로
+# title(): 단어마다 첫 글자를 대문자로
